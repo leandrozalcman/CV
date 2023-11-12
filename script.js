dc67906981cf23a963e5c467413686818df61d00
leandrozalcman <142274722+leandrozalcman@users.noreply.github.com>
2023-11-12 16:13:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function toggleDarkMode() {
+    var body = document.getElementsByTagName("body")[0];
+    var textBox = document.querySelector(".text-box");
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        textBox.style.color = "white";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/blue-wallpaper-with-white-lines_483537-4468.jpg?w=1380&t=st=1699752686~exp=1699753286~hmac=22f9489606ad7198953aa1aefba6c8d009bc17a1bab34e64f385e96c4be8280d')";
+        localStorage.setItem("theme", "dark");
+    } else {
+        textBox.style.color = "black";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/blue-geometric-frame-vector_53876-140352.jpg')";
+        localStorage.setItem("theme", "light");
+    }
+    var darkModeButton = document.querySelector(".dark-mode");
+    if (body.classList.contains("dark")) {
+        darkModeButton.innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        darkModeButton.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+}
+
+function toggleMenu() {
+    var x = document.getElementsByClassName("menu")[0];
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+
+window.onload = function () {
+    var theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.getElementsByTagName("body")[0].classList.add("dark");
+        document.querySelector(".text-box").style.color = "white";
+        document.querySelector(".background-image").style.backgroundImage = "url('https://img.freepik.com/free-vector/blue-wallpaper-with-white-lines_483537-4468.jpg?w=1380&t=st=1699752686~exp=1699753286~hmac=22f9489606ad7198953aa1aefba6c8d009bc17a1bab34e64f385e96c4be8280d')";
+        document.querySelector(".dark-mode").innerHTML = '<i class="fas fa-moon"></i>';
+    }
+};
